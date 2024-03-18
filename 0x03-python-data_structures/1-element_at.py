94312f367c069885853521423424d9f34535a13a
Abu Jehoiada <abujehoiada@gmail.com>
2024-03-18 03:38:18 -0700

    Update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0: #Check for negative index
+    # Check for negative index
+    if idx < 0:
         return None
-    elif idx > len(my_list): #Check for out of range of idx
+    # Check for out of range of idx
+    elif idx > len(my_list):
         return None
     else:
         return idx + 1
