cb851d0b2c7e88946b14e9c7e86189613ff77c11
Abu Jehoiada <abujehoiada@gmail.com>
2024-03-20 17:21:40 +0000

    Update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for i in matrix:
-        squared = i * 2
+    # Creates a new matrix with same size as input matrix using list comprehension
+    squared = [[x**2 for x in row]for row in matrix]
     return squared
