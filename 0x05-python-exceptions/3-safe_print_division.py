184732cd82495273e9a9168b34bdf776d682972a
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-02 05:56:58 -0700

    Integers division with debug Correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except Exception:
         result = None
     finally:
-            print("Inside result: {}".format(result))
-            return result
+        print("Inside result: {}".format(result))
+        return result
