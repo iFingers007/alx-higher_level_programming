a9eca16f414112cdae9a189c8bc3e3406b6dd3af
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-02 18:53:58 -0700

    Update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class that defines Square"""
+
+
+class Square:
+    """A class that defines Square"""
+    def __init__(self, size):
+        """__init__ method
+
+        Args:
+        size: size of Square
+        """
+        self.__size = size
