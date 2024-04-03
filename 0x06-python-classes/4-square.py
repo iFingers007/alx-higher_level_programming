1d930c0eef150fcd64ce72c5e7da859248c897a6
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-03 16:22:31 -0700

    Update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a class that defines Square"""
+
+
+class Square:
+    """A class that defines Square"""
+    def __init__(self, size=0):
+        """__init__ method
+
+        Args:
+        size: size of Square
+        """
+
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("Size must be an integer")
+            if value < 0:
+                raise ValueError("Size must be >= 0")
+            self.__size = value
+
+
+    def area(self):
+        """ Public instance that returns
+        area
+        """
+        product = (self.__size) * (self.__size)
+        return product
