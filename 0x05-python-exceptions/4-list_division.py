8066a04cc4ba0fad79130e8648e311d474cdaeb2
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-02 19:02:35 -0700

    Fixed List Division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    if not my_list_1 or not my_list_2:
-        print("Input lists cannot be empty")
-        return []
     res = []
-    try:
-        for i in range(list_length):
-            num1 = my_list_1[i]
-            num2 = my_list_2[i]
-            try:
-                res.append(num1 / num2)
-            except TypeError:
-                print("wrong type")
-                res.append(0)
-            except ZeroDivisionError:
-                print("division by 0")
-                res.append(0)
-    except IndexError:
-        print("out of range")
-        res.append(0)
-    except ValueError:
-        print("list cannot be Empty")
-    finally:
-        return res
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+            res.append(res)
+        except TypeError:
+            print("wrong type")
+            res.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            res.append(0)
+        except IndexError:
+            print("out of range")
+            res.append(0)
+        finally:
+            continue
+    return res
