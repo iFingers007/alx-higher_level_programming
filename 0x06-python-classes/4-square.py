e70e6303035eacf5df29a5e9fc2c249aba256743
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-03 16:33:07 -0700

    Update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,14 +16,14 @@ class Square:
         @property
         def size(self):
             return self.__size
+
         @size.setter
         def size(self, value):
             if not isinstance(value, int):
-                raise TypeError("Size must be an integer")
+                raise TypeError("size must be an integer")
             if value < 0:
-                raise ValueError("Size must be >= 0")
+                raise ValueError("size must be >= 0")
             self.__size = value
-
 
     def area(self):
         """ Public instance that returns
