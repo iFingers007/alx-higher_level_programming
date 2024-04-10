aaf23a4ea13cc68f1b68fe2f5271086189613c06
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-09 17:46:43 -0700

    Update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" Module for Text_indentation function
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    parameters:
+    text (str) = String text
+
+    Raises:
+    TypeError: If text is not a string.
+    """
+
+    spec_chars = ['.', '?', ':']
+    lines = []
+    curr_line = ""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in text:
+        if char in spec_chars:
+            lines.append(curr_line.strip())
+            lines.append("")
+            curr_line = ""
+        else:
+            curr_line += char
+    if curr_line:
+        lines.append(curr_line.strip())
+    for i, line in enumerate(lines):
+        print(line, end='')
+        if i < len(lines) -1:
+            print()
