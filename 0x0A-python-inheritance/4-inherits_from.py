8d5fbcf3571ae9442c7e7152cfd98b10a3093778
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-15 15:39:47 -0700

    Fixed code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,7 +18,7 @@ def inherits_from(obj, a_class):
     bool: True or false
 
     """
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     else:
         return False
