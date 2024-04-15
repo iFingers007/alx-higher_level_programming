2662b468404ea6e04d75ebebd15d4c45fd2cacda
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-15 10:22:30 -0700

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,5 +11,7 @@ class MyList(list):
     """ List subclass that inherits list attributes
     """
     def print_sorted(self):
+        """ Prints the list, but sorted
+        """
         sorted_list = sorted(self)
         print(sorted_list)
