b5e6beb851d6ad2e3d77ee4459f00d2b6fc41b0b
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-15 11:02:37 -0700

    Fixed code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,10 @@
 
 class MyList(list):
     """ List subclass that inherits list attributes
+
+    Args:
+    list: Super class
+
     """
     def print_sorted(self):
         """ Prints the list, but sorted
