72e933e6d6270e6b659384809eeaaf556d6d2d8b
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-18 17:27:03 -0700

    Fixed code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 """Module for the Class Base"""
-
 
 class Base:
     """Base class of the project"""
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,94 @@
-"""
-"""Module for the Class Base"""
+#!/usr/bin/python3
+"""Module for the subClass Rectangle"""
+from models.base import Base
 
 
-class Base:
-    """Base class of the project"""
-    __nb_objects = 0
+class Rectangle(Base):
+    """Rectangle subclass of the project"""
 
-    def __init__(self, id=None):
-        """Initialisation of the Base class"""
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialisation function for the class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getting value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getting value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getting value of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setting value of x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getting value of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setting value of y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Computes the area of a rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Displays the rectangle using #"""
+        for i in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """String representation of class"""
+        return (f"[Rectangle] ({self.id}) "
+                f"{self.x}/{self.y} - {self.width}/{self.height}")
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args:
+            attr = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attr[i], arg)
