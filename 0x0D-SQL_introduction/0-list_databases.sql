1ede560ae9e1e0be1b109c392c25a986766b2e90
Abu Jehoiada <abujehoiada@gmail.com>
2024-05-21 20:13:19 -0700

    Beginning to End

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Script that lists all databases of your MySQL server
+-- The SHOW keyword will be used
+SHOW DATABASES
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- A script that displays the number of records with id = 89
+-- in the table first_table
+SELECT COUNT(*) AS Number_of_count
+FROM first_table
+WHERE id=89
