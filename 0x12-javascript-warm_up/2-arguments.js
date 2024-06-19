22ca9c686a143fe241659db0d33075482c438db2
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-19 21:02:23 +0100

    Fixed code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,6 @@ if (argsCount <= 2) {
   console.log('No Argument');
 } else if (argsCount === 3) {
   console.log('Argument found');
-} else {
+} else if (argsCount > 3) {
   console.log('Arguments found');
 }
