f29d97723c3113ed3a0bd1dd6013aa811da9287e
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-19 22:57:59 +0100

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const size = parseInt(args[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    const string = 'X';
+    const repeated = string.repeat(size);
+    console.log(`${repeated}`);
+  }
+}
