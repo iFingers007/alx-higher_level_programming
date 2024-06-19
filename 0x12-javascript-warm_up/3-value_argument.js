ca1a01560c6e85a1e109cd95a3c8aa1c6d87d436
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-19 21:13:42 +0100

    Fixed code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,5 +4,5 @@ const args = process.argv.slice(2);
 if (Array.isArray(args) && args[0] !== undefined) {
   console.log(args[0]);
 } else {
-  console.log("No Argument");
+  console.log('No Argument');
 }
