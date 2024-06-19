666074fcbf77097fa63b2939024749c8343bb454
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-19 21:58:49 +0100

    Fixed code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,4 @@
 const args = process.argv;
 const argsNum = parseInt(args[2]);
 
-if (isNaN(argsNum)) { console.log('Not a number'); } else { console.log(`My Number: ${argsNum}`); }
+if (isNaN(argsNum)) { console.log('Not a number'); } else { console.log('My Number: ' + `${argsNum}`); }
