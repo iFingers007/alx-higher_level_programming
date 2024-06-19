f42ebcf85de2cf261dd7d125eb9aa3d071aa8ef4
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-19 20:56:07 +0100

    Fixed code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const args = process.argv.length - 2;
 
-if (args.length === 0) {
+if (args === 0) {
   console.log('No Argument');
-} else if (args.length === 1) {
+} else if (args === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
