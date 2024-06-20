ec1ae9d677f8dd2ac37a7108c021b1b27c71e806
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-20 01:54:29 +0100

    Fixed code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const args = process.argv;
 
 if (args.length <= 3) {
-    console.log(0)
+  console.log(0);
 } else {
-    const numArray = args.map(str => parseInt(str));
-    const sortedArray = numArray.sort((a, b) => b - a );
-    console.log(sortedArray[3]);
+  const numArray = args.map(str => parseInt(str));
+  const sortedArray = numArray.sort((a, b) => b - a);
+  console.log(sortedArray[3]);
 }
