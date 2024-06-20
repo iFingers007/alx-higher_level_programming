fa89601a6157656e3b0477a58caf4a7a650d2a12
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-20 01:10:46 +0100

    Fixed code

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 0) {
+    return 1;
+  }
+  if (isNaN(num)) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const arg = process.argv[2];
+const result = factorial(arg);
+console.log(result);
