f55384cf8547707730de672ff9e744fdb5980c67
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-21 02:35:10 +0100

    Fixed code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,15 @@
 
 const SquareBase = require('./5-square');
 
-class Square extends SquareBase{
-    charPrint (c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            console.log(c.repeat(this.width));
-        }
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
