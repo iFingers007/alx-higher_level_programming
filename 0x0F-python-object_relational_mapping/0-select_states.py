bbc2ffa042ed337aef696da3ba45250644d36d3a
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-22 11:09:14 +0100

    Fixed code

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module for Select State Database"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=db_username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print (row)
+
+    cursor.close()
+    db.close()
