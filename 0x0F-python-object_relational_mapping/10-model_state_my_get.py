d52977d5a91cc68ca71d3b25a19957a996ae6537
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-25 14:28:02 -0700

    Fixed code

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Module for State listing"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    usr, pw, db, sn = sys.argv[1:5]
+    host = 'localhost'
+    port = 3306
+    par = f'mysql+mysqldb://{usr}:{pw}@localhost/{db}'
+
+    engine = create_engine(par, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    match = session.query(State).filter(State.name == sn).first()
+
+    if match:
+        print(f"{match.id}")
+    else:
+        print("Not Found")
