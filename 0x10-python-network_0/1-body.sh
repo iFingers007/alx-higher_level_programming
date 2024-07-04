aa3dd100e617a94b571773cbc4c34dd9843e6e65
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-04 13:15:11 -0700

    task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o temp_body -w "%{http_code}" "$1" | grep -q 200 && cat temp_body; rm temp_body
+curl -sL "$1"
