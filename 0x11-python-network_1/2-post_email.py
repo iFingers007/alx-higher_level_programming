68ee39d6fc8ece9adcce2fc7ed788496d2ebc56d
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-05 09:49:09 -0700

    Post an Email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python Network 1 Module"""
 
-import urllib
+import urllib.parse
+import urllib.request
 import sys
 
 if __name__ == "__main__":
     url, email = sys.argv[1], sys.argv[2]
-    data = urllib.parse.urlencode({'email:' email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
     with urllib.request.urlopen(url, data) as resp:
         data = resp.read().decode('utf-8')
