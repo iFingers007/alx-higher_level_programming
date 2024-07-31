82755c48a4f4d1498cb6f0ba63e4e428c7f83158
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-31 04:08:03 +0100

    Fixed code

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[2];
+
+if (!url || !filePath) {
+  console.error('Error: Provide url and file path');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        console.log('Error writing to file:', err);
+      }
+    });
+  } else {
+    console.error('Failed to retrieve webpage:', error);
+  }
+});
