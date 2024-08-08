5a258e9beb9b672431f9bea5c9336efa291471de
Abu Jehoiada <abujehoiada@gmail.com>
2024-08-08 01:36:38 +0100

    Added New Task and Fixed semistandard

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    const headerElement = $('header');
+    if ($(headerElement).hasClass('red')) {
+      $(headerElement).removeClass('red').addClass('green');
+    } else {
+      $(headerElement).removeClass('green').addClass('red');
+    }
+  });
+});
