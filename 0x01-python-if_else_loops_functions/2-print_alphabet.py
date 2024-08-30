909c7aa757e64a2269914e975f4b753819f6d6a2
Abu Jehoiada <abujehoiada@gmail.com>
2024-08-30 03:49:10 +0100

    Fixed code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # for letters in range(ord('a'), ord('z') + 1):
-#    print('{}'.format(chr(letters)), end='')
+
 
 for i in range(97, 123):
     print('{}'.format(chr(i)), end="")
