c0069de91012f7a70d5359ae55c2c26ac8ffd7e0
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-23 18:59:24 +0100

    Redoing  6-print_sorted_dictionary.py to making it simpler

--- 0x04-python-more_data_structures/6-main.py
+++ 0x04-python-more_data_structures/6-main.py
@@ -2,4 +2,13 @@
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+my_dict = { }
+my_dict_0 = { 'languages': "C" }
+my_dict_1 = { 'a': "e", 'B': "d" , 'c': "c", 'd': "b", 'E': "a" }
+print_sorted_dictionary(my_dict)
+print("----")
+print_sorted_dictionary(my_dict_0)
+print("----")
+print_sorted_dictionary(my_dict_1)
+print("----")
 print_sorted_dictionary(a_dictionary)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for k, v in sorted(a_dictionary.items()):
-        print(f"{k}:{v}")
+    for k in sorted(a_dictionary):
+        print(f"{k}:{a_dictionary[k]}")
     # Step 1: Extract Keys
 #    keys = list(a_dictionary.keys())
     # 2: Sort Keys
