9015cf9d4180b7df7fd335a687a2cdc6db1332db
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-23 17:23:35 +0100

    Redoing  6-print_sorted_dictionary.py to making it simpler

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    for k, v in sorted(a_dictionary.items()):
+        print(f"{k}:{v}")
     # Step 1: Extract Keys
-    keys = list(a_dictionary.keys())
+#    keys = list(a_dictionary.keys())
     # 2: Sort Keys
-    keys.sort()
+#    keys.sort()
     # 3: Iterate over loop and print key, value pair
-    for key in keys:
-        print(key + ": " + str(a_dictionary[key]))
+#    for key in keys:
+#        print(key + ": " + str(a_dictionary[key]))
