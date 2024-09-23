102df295b23c813a30e564a7e9495f08cd2afada
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-23 19:01:04 +0100

    Redoing  6-print_sorted_dictionary.py to making it simpler

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for k in sorted(a_dictionary):
-        print(f"{k}:{a_dictionary[k]}")
+    [print(f"{k}:{a_dictionary[k]}") for k in sorted(a_dictionary)]
     # Step 1: Extract Keys
 #    keys = list(a_dictionary.keys())
     # 2: Sort Keys
