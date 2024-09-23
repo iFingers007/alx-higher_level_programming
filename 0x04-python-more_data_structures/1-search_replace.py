f02e61cbf5363910b635a19eb280bbe6204b5827
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-23 15:19:16 +0100

    Fixed search and replace Task using list comp

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for word in my_list:
-        if word == search:
-            new_list.append(replace)
-        else:
-            new_list.append(word)
-    return new_list
+    ''' Using List comprehension '''
+    return [replace if elm == search else elm for elm in my_list]
+#    new_list = []
+#    for word in my_list:
+#        if word == search:
+#            new_list.append(replace)
+#        else:
+#            new_list.append(word)
+#    return new_list
