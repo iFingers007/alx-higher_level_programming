f1e93b46e8e6b1d48ba6b9d5cff8ff9f10457d6c
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-23 16:05:39 +0100

    Fixed code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+# def uniq_add(my_list=[]):
+#    uniques = set()
+#    tSum = 0
+#    for elmt in my_list:
+#        if elmt not in uniques:
+#            uniques.add(elmt)
+#            tSum += elmt
+#    return tSum
 def uniq_add(my_list=[]):
-    uniques = set()
-    tSum = 0
-    for elmt in my_list:
-        if elmt not in uniques:
-            uniques.add(elmt)
-            tSum += elmt
-    return tSum
+    sum_set = set(my_list)
+    return sum(sum_set)
