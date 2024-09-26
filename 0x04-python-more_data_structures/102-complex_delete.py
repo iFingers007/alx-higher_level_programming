faf8073cab36b6bbaba68e314d51753dc4c18a14
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-26 01:08:48 +0100

    Added 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,7 +6,6 @@ def complex_delete(a_dictionary, value):
     for key, val in a_dictionary.items():
         if val == value:
             keys_to_pop.append(key)
-    print(keys_to_pop)
     for k in keys_to_pop:
         a_dictionary.pop(k)
     return a_dictionary
