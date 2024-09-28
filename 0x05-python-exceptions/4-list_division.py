df8e3ab92b3499bed7c5d5ca0ab26325e3b6305b
Abu Jehoiada <abujehoiada@gmail.com>
2024-09-28 15:09:17 +0100

    refactored 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    res = []
-    for i in range(list_length):
-        try:
-            result = my_list_1[i] / my_list_2[i]
-            res.append(result)
-        except TypeError:
-            print("wrong type")
-            res.append(0)
-        except ZeroDivisionError:
-            print("division by 0")
-            res.append(0)
-        except IndexError:
-            print("out of range")
-            res.append(0)
-        finally:
-            continue
-    return res
+    result = []
+    try:
+        for i in range(list_length):
+            try:
+                result.append(my_list_1[i] / my_list_2[i])
+            except (TypeError, ValueError):
+                print("wrong type")
+                result.append(0)
+            except ZeroDivisionError:
+                print("division by zero")
+                result.append(0)
+    except IndexError:
+                print("out of range")
+                result.append(0)
+    finally:
+        return result
+#    res = []
+#    for i in range(list_length):
+#        try:
+#            result = my_list_1[i] / my_list_2[i]
+#            res.append(result)
+#        except TypeError:
+#            print("wrong type")
+#            res.append(0)
+#        except ZeroDivisionError:
+#            print("division by 0")
+#            res.append(0)
+#        except IndexError:
+#            print("out of range")
+#            res.append(0)
+#        finally:
+#            continue
+#    return res
